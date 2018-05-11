79608420690f93729d829d41f2f47960550407aa
Nick (Partixel) Lock <partixel@v-handle.com>
2018-05-11 12:52:17 +0100

    Fixed arm welds with R15 not working correctly

--- MainModule.ModuleScript/ReplicatedStorage.Folder/Core.ModuleScript/ArmUtil.ModuleScript.lua
+++ MainModule.ModuleScript/ReplicatedStorage.Folder/Core.ModuleScript/ArmUtil.ModuleScript.lua
@@ -1,58 +1,30 @@
 local new = Instance.new
 
-local Strs = { { "Left Shoulder", "Left Arm", "Left_Weld" }, { "Right Shoulder", "Right Arm", "Right_Weld" } }
-
-local Strs2 = { { "LeftShoulder", "LeftUpperArm", "Left_Weld" }, { "RightShoulder", "RightUpperArm", "Right_Weld" } }
-
-local function UnWeld( Plr, Tool )
+function GetArmWelds( Char )
 	
-	local Character = Plr.Character
-	
-	if Plr.Character == nil then return end
-	
-	local Torso = Character:FindFirstChild( "Torso" )
-	
-	if not Torso then
+	if Char:FindFirstChild( "Torso" ) then
 		
-		Torso = Character:FindFirstChild( "UpperTorso" )
+		return Char.Torso:FindFirstChild( "Left Shoulder" ), Char.Torso:FindFirstChild( "Right Shoulder" )
 		
-		for a = 1, 2 do
-			
-			if Character:FindFirstChild( Strs2[ a ][ 2 ] ) and Character[ Strs2[ a ][ 2 ] ]:FindFirstChild( Strs2[ a ][ 1 ] ) then
-				
-				Character[ Strs2[ a ][ 2 ] ][ Strs2[ a ][ 1 ] ].Part1 = Character[ Strs2[ a ][ 2 ] ]
-				
-				if Torso:FindFirstChild( Strs2[ a ][ 3 ] ) then
-					
-					Torso[ Strs2[ a ][ 3 ] ]:Destroy( )
-					
-				end
-				
-			end
-			
-		end
+	else
 		
-		return
+		return Char:FindFirstChild( "LeftUpperArm" ) and Char.LeftUpperArm:FindFirstChild( "LeftShoulder" ), Char:FindFirstChild( "RightUpperArm" ) and Char.RightUpperArm:FindFirstChild( "RightShoulder" ), Char:FindFirstChild( "LeftLowerArm" ) and Char.LeftLowerArm:FindFirstChild( "LeftElbow" ), Char:FindFirstChild( "RightLowerArm" ) and Char.RightLowerArm:FindFirstChild( "RightElbow" )
 		
 	end
 	
-	if Torso then
+end
+
+local Welds = { }
+
+local function UnWeld( Plr, Tool )
+	
+	for a, b in pairs( Welds ) do
 		
-		for a = 1, 2 do
-			
-			if Torso:FindFirstChild( Strs[ a ][ 1 ] ) and Character:FindFirstChild( Strs[ a ][ 2 ] ) then
-				
-				Torso[ Strs[ a ][ 1 ] ].Part1 = Character[ Strs[ a ][ 2 ] ]
-				
-				if Torso:FindFirstChild( Strs[ a ][ 3 ] ) then
-					
-					Torso[ Strs[ a ][ 3 ] ]:Destroy( )
-					
-				end
-				
-			end
-			
-		end
+		b.Part1 = a.Part1
+		
+		a:Destroy( )
+		
+		Welds[ a ] = nil
 		
 	end
 	
@@ -60,151 +32,103 @@ end
 
 local function WeldArms( Plr, Tool, CF1, CF2  )
 	
-	local Character = Plr.Character
+	if Plr.Character == nil then return end
 	
-	if Character == nil then return end
+	local Char = Plr.Character
 	
-	local Torso = Character:FindFirstChild( "Torso" )
+	local LS, RS, LE, RE = GetArmWelds( Char )
 	
-	if not Torso then
+	if LS then
 		
-		Torso = Character:FindFirstChild( "UpperTorso" )
+		local Weld = new( "Weld" )
 		
-		for a = 1, 2 do
-			
-			if ( a == 1 and CF1 ) or ( a == 2 and CF2 ) then
-				
-				if Character:FindFirstChild( Strs2[ a ][ 2 ] ) and Character[ Strs2[ a ][ 2 ] ]:FindFirstChild( Strs2[ a ][ 1 ] ) then
-					
-					Character[ Strs2[ a ][ 2 ] ][ Strs2[ a ][ 1 ] ].Part1 = nil
-					
-					local Weld = new( "Weld" )
-					
-					Weld.Name = Strs2[ a ][ 3 ]
-					
-					Weld.Part0 = Torso
-					
-					Weld.Part1 = Character[ Strs2[ a ][ 2 ] ]
-					
-					if a == 1 then
-						
-						Weld.C1 = CF1
-						
-					else
-						
-						Weld.C1 = CF2
-						
-					end
-					
-					Weld.Parent = Torso
-					
-				end
-				
-			end
-			
-		end
+		Weld.Name = "ArmWeld"
 		
-		return
+		Weld.Part0 = LS.Part0
+		
+		Weld.Part1 = LS.Part1
+		
+		LS.Part1 = nil
+		
+		--Weld.C0 = LS.C0
+		
+		Weld.C1 = CF1
+		
+		Welds[ Weld ] = LS
+		
+		Weld.Parent = Tool.Handle
 		
 	end
 	
-	for a = 1, 2 do
+	if RS then
 		
-		if ( a == 1 and CF1 ) or ( a == 2 and CF2 ) then
-			
-			if Torso:FindFirstChild( Strs[ a ][ 1 ] ) and Character:FindFirstChild( Strs[ a ][ 2 ] ) then
-				
-				Torso[ Strs[ a ][ 1 ] ].Part1 = nil
-				
-				local Weld = new( "Weld" )
-				
-				Weld.Name = Strs[ a ][ 3 ]
-				
-				Weld.Part0 = Torso
-				
-				Weld.Part1 = Character[ Strs[ a ][ 2 ] ]
-				
-				if a == 1 then
-					
-					Weld.C1 = CF1
-					
-				else
-					
-					Weld.C1 = CF2
-					
-				end
-				
-				Weld.Parent = Torso
-				
-			end
-			
-		end
+		local Weld = new( "Weld" )
+		
+		Weld.Name = "ArmWeld"
+		
+		Weld.Part0 = RS.Part0
+		
+		Weld.Part1 = RS.Part1
+		
+		RS.Part1 = nil
+		
+		--Weld.C0 = RS.C0
+		
+		Weld.C1 = CF2
+		
+		Welds[ Weld ] = RS
+		
+		Weld.Parent = Tool.Handle
 		
 	end
 	
-end
-
-local function NewWeld( Plr, Tool, CF1  )
-	
-	local Character = Plr.Character
-	
-	if Character == nil then return end
-	
-	local Torso = Character:FindFirstChild( "Torso" )
-	
-	if not Torso then
+	if LE then
 		
-		Torso = Character:FindFirstChild( "UpperTorso" )
+		local Weld = new( "Weld" )
 		
-		if Torso:FindFirstChild( Strs[ 2 ][ 1 ] ) and Character:FindFirstChild( Strs[ 2 ][ 2 ] ) then
-			
-			Torso[ Strs[ 2 ][ 1 ] ].Part1 = nil
-			
-			local Weld = new( "Weld" )
-			
-			Weld.Name = Strs[ 2 ][ 3 ]
-			
-			Weld.Part0 = Torso
-			
-			Weld.Part1 = Character[ Strs[ 2 ][ 2 ] ]
-			
-			Weld.C1 = CF1
-			
-			Weld.Parent = Torso
-			
-		end
+		Weld.Name = "ArmWeld"
 		
-		return
+		Weld.Part0 = LE.Part0
+		
+		Weld.Part1 = LE.Part1
+		
+		LE.Part1 = nil
+		
+		Weld.C0 = LE.C0
+		
+		Weld.C1 = LE.C1
+		
+		Welds[ Weld ] = LE
+		
+		Weld.Parent = Tool.Handle
 		
 	end
 	
-	if Torso:FindFirstChild( Strs[ 1 ][ 1 ] ) and Character:FindFirstChild( Strs[ 1 ][ 2 ] ) then
+	if RE then
 		
-		game["Run Service"].Stepped:Connect( function ( )
-			
-			if Tool:FindFirstChild( "Handle" ) and Tool.Handle:FindFirstChild( "LeftTarget" ) then
-				
-				local LocalTarget = ( Torso.CFrame + Torso[ Strs[ 1 ][ 1 ] ].C0.p ):pointToObjectSpace( Tool.Handle.LeftTarget.WorldPosition )
-				
-				Torso[ Strs[ 1 ][ 1 ] ].Transform = CFrame.new( Vector3.new( ), LocalTarget )
-				
-			end
-			
-		end )
+		local Weld = new( "Weld" )
+		
+		Weld.Name = "ArmWeld"
+		
+		Weld.Part0 = RE.Part0
+		
+		Weld.Part1 = RE.Part1
+		
+		RE.Part1 = nil
+		
+		Weld.C0 = RE.C0
+		
+		Weld.C1 = RE.C1
+		
+		Welds[ Weld ] = RE
+		
+		Weld.Parent = Tool.Handle
 		
 	end
 	
 end
 
 return function ( Plr, Tool, CF1, CF2 )
-	
-	if Tool:WaitForChild( "Handle" ):FindFirstChild( "LeftTarget" ) then
-		
-		Tool.Equipped:Connect( function ( ) NewWeld( Plr, Tool, CF2 ) end )
-		
-		return
-		
-	end
 	
 	Tool.Equipped:Connect( function ( ) WeldArms( Plr, Tool, CF1, CF2 ) end )
 	
