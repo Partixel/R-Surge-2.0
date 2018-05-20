23914601615729c6d984ff1291abc3fe61de68cb
Nick Lock <partixel@v-handle.com>
2018-05-20 23:38:04 +0100

    Fixed certain hats not hiding in first person

--- MainModule.ModuleScript/StarterCharacterScripts.Folder/FirstPersonMod.LocalScript.lua
+++ MainModule.ModuleScript/StarterCharacterScripts.Folder/FirstPersonMod.LocalScript.lua
@@ -13,7 +13,7 @@ function HandlePart( Part )
 		local c7 = Part.CFrame * CFrame.new(Part.Size.X / 2, -Part.Size.Y / 2, Part.Size.Z / 2)
 		local c8 = Part.CFrame * CFrame.new(-Part.Size.X / 2, Part.Size.Y / 2, -Part.Size.Z / 2)
 		
-		if math.max( c1.Y, c2.Y, c3.Y, c4.Y, c5.Y, c6.Y, c7.Y, c8.Y ) - 0.15 < workspace.CurrentCamera.Focus.Y then
+		if math.max( c1.Y, c2.Y, c3.Y, c4.Y, c5.Y, c6.Y, c7.Y, c8.Y ) < workspace.CurrentCamera.Focus.Y - 0.45 then
 			
 			Part.LocalTransparencyModifier = 0
 			
