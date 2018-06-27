a7238905d363229352ccbd5c6e0ba844b6852d09
Nick (Partixel) Lock <partixel@v-handle.com>
2018-06-27 20:45:53 +0100

    Fixed torso and arms dissapearing in first person with R15

--- MainModule.ModuleScript/StarterCharacterScripts.Folder/FirstPersonMod.LocalScript.lua
+++ MainModule.ModuleScript/StarterCharacterScripts.Folder/FirstPersonMod.LocalScript.lua
@@ -1,5 +1,5 @@
 function HandlePart( Part )
-	
+	print( Part )
 	local Event Event = Part:GetPropertyChangedSignal( "LocalTransparencyModifier" ):Connect( function ( )
 		
 		if not Part:IsDescendantOf( script.Parent ) then Event:Disconnect( ) return end
@@ -13,7 +13,7 @@ function HandlePart( Part )
 		local c7 = Part.CFrame * CFrame.new(Part.Size.X / 2, -Part.Size.Y / 2, Part.Size.Z / 2)
 		local c8 = Part.CFrame * CFrame.new(-Part.Size.X / 2, Part.Size.Y / 2, -Part.Size.Z / 2)
 		
-		if math.max( c1.Y, c2.Y, c3.Y, c4.Y, c5.Y, c6.Y, c7.Y, c8.Y ) < workspace.CurrentCamera.Focus.Y - 0.45 then
+		if math.max( c1.Y, c2.Y, c3.Y, c4.Y, c5.Y, c6.Y, c7.Y, c8.Y ) < ( workspace.CurrentCamera.Focus - workspace.CurrentCamera.Focus.upVector * 0.2 ).Y then
 			
 			Part.LocalTransparencyModifier = 0
 			
@@ -23,9 +23,13 @@ function HandlePart( Part )
 	
 end
 
+local Handled = setmetatable( { }, { __mode = "k" } )
+
 script.Parent.DescendantAdded:Connect( function ( Obj )
 	
-	if Obj:IsA( "BasePart" ) then
+	if not Handled[ Obj ] and Obj:IsA( "BasePart" ) then
+		
+		Handled[ Obj ] = true
 		
 		HandlePart( Obj )
 		
@@ -37,7 +41,9 @@ local Kids = script.Parent:GetDescendants( )
 
 for a = 1, #Kids do
 	
-	if Kids[ a ]:IsA( "BasePart" ) then
+	if not Handled[ Kids[ a ] ] and  Kids[ a ]:IsA( "BasePart" ) then
+		
+		Handled[ Kids[ a ] ] = true
 		
 		HandlePart( Kids[ a ] )
 		
