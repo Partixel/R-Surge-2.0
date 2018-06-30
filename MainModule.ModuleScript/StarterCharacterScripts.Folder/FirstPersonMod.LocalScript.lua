1803107285ffd9163caf873476fc47a1b569b709
Nick (Partixel) Lock <partixel@v-handle.com>
2018-06-30 20:28:38 +0100

    Only make legs and arms visible in first person to stop torso and such blocking your view

--- MainModule.ModuleScript/StarterCharacterScripts.Folder/FirstPersonMod.LocalScript.lua
+++ MainModule.ModuleScript/StarterCharacterScripts.Folder/FirstPersonMod.LocalScript.lua
@@ -4,20 +4,7 @@ function HandlePart( Part )
 		
 		if not Part:IsDescendantOf( script.Parent ) then Event:Disconnect( ) return end
 		
-		local c1 = Part.CFrame * CFrame.new(Part.Size.X / 2, Part.Size.Y / 2, Part.Size.Z / 2)
-		local c2 = Part.CFrame * CFrame.new(-Part.Size.X / 2, Part.Size.Y / 2, Part.Size.Z / 2)
-		local c3 = Part.CFrame * CFrame.new(-Part.Size.X / 2, -Part.Size.Y / 2, Part.Size.Z / 2)
-		local c4 = Part.CFrame * CFrame.new(-Part.Size.X / 2, -Part.Size.Y / 2, -Part.Size.Z / 2)
-		local c5 = Part.CFrame * CFrame.new(Part.Size.X / 2, -Part.Size.Y / 2, -Part.Size.Z / 2)
-		local c6 = Part.CFrame * CFrame.new(Part.Size.X / 2, Part.Size.Y / 2, -Part.Size.Z / 2)
-		local c7 = Part.CFrame * CFrame.new(Part.Size.X / 2, -Part.Size.Y / 2, Part.Size.Z / 2)
-		local c8 = Part.CFrame * CFrame.new(-Part.Size.X / 2, Part.Size.Y / 2, -Part.Size.Z / 2)
-		
-		if math.max( c1.Y, c2.Y, c3.Y, c4.Y, c5.Y, c6.Y, c7.Y, c8.Y ) < ( workspace.CurrentCamera.Focus - workspace.CurrentCamera.Focus.upVector * 0.2 ).Y then
-			
-			Part.LocalTransparencyModifier = 0
-			
-		end
+		Part.LocalTransparencyModifier = 0
 		
 	end )
 	
@@ -27,7 +14,7 @@ local Handled = setmetatable( { }, { __mode = "k" } )
 
 script.Parent.DescendantAdded:Connect( function ( Obj )
 	
-	if not Handled[ Obj ] and Obj:IsA( "BasePart" ) then
+	if not Handled[ Obj ] and Obj:IsA( "BasePart" ) and ( Obj.Name:lower( ):find( "leg" ) or Obj.Name:lower( ):find( "arm" ) ) then
 		
 		Handled[ Obj ] = true
 		
@@ -41,7 +28,7 @@ local Kids = script.Parent:GetDescendants( )
 
 for a = 1, #Kids do
 	
-	if not Handled[ Kids[ a ] ] and  Kids[ a ]:IsA( "BasePart" ) then
+	if not Handled[ Kids[ a ] ] and Kids[ a ]:IsA( "BasePart" ) and ( Kids[ a ].Name:lower( ):find( "leg" ) or Kids[ a ].Name:lower( ):find( "arm" ) ) then
 		
 		Handled[ Kids[ a ] ] = true
 		
