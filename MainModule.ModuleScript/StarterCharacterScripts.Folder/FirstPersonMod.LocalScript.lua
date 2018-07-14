2a586bdb10d8c13eb31463ecb796767fe6bc2ea4
Nick (Partixel) Lock <partixel@v-handle.com>
2018-07-14 20:09:56 +0100

    Fixed feet and hands not showing in first person for R15

--- MainModule.ModuleScript/StarterCharacterScripts.Folder/FirstPersonMod.LocalScript.lua
+++ MainModule.ModuleScript/StarterCharacterScripts.Folder/FirstPersonMod.LocalScript.lua
@@ -14,7 +14,7 @@ local Handled = setmetatable( { }, { __mode = "k" } )
 
 script.Parent.DescendantAdded:Connect( function ( Obj )
 	
-	if not Handled[ Obj ] and Obj:IsA( "BasePart" ) and ( Obj.Name:lower( ):find( "leg" ) or Obj.Name:lower( ):find( "arm" ) ) then
+	if not Handled[ Obj ] and Obj:IsA( "BasePart" ) and ( Obj.Name:lower( ):find( "leg" ) or Obj.Name:lower( ):find( "arm" ) or Obj.Name:lower( ):find( "hand" ) or Obj.Name:lower( ):find( "foot" ) ) then
 		
 		Handled[ Obj ] = true
 		
@@ -28,7 +28,7 @@ local Kids = script.Parent:GetDescendants( )
 
 for a = 1, #Kids do
 	
-	if not Handled[ Kids[ a ] ] and Kids[ a ]:IsA( "BasePart" ) and ( Kids[ a ].Name:lower( ):find( "leg" ) or Kids[ a ].Name:lower( ):find( "arm" ) ) then
+	if not Handled[ Kids[ a ] ] and Kids[ a ]:IsA( "BasePart" ) and ( Kids[ a ].Name:lower( ):find( "leg" ) or Kids[ a ].Name:lower( ):find( "arm" ) or Kids[ a ].Name:lower( ):find( "hand" ) or Kids[ a ].Name:lower( ):find( "foot" ) ) then
 		
 		Handled[ Kids[ a ] ] = true
 		
