969485d04f57ac2cc5d3bc91994cca543cc483fc
Nick (Partixel) Lock <partixel@v-handle.com>
2019-09-24 20:41:59 +0100

    Optimised DropHat + added "s2nodrop" tag to stop dropping a hat

--- MainModule.ModuleScript/ServerScriptService.Folder/DropHat.Script.lua
+++ MainModule.ModuleScript/ServerScriptService.Folder/DropHat.Script.lua
@@ -1,51 +1,32 @@
-local Core = require( game:GetService( "ReplicatedStorage" ):WaitForChild( "S2" ):WaitForChild( "Core" ) )
+local CollectionService = game:GetService("CollectionService")
+local Core = require(game:GetService("ReplicatedStorage"):WaitForChild("S2"):WaitForChild("Core"))
 
-local DropHatRemote = Instance.new( "RemoteEvent" )
-
+local DropHatRemote = Instance.new("RemoteEvent")
 DropHatRemote.Name = "DropHatRemote"
 
-DropHatRemote.OnServerEvent:Connect( function ( Plr )
-
+DropHatRemote.OnServerEvent:Connect(function(Plr)
 	if Core.Config.HatMode == 1 or not Plr.Character then return end
-
-	local Hats = Plr.Character:GetChildren( )
-
-	for a = 1, #Hats do
-
-		if Hats[ a ]:IsA( "Accessory" ) then
-
+	
+	for _, Hat in ipairs(Plr.Character:GetChildren()) do
+		if Hat:IsA("Accessory") and not CollectionService:HasTag(Hat, "s2nodrop") then
 			if Core.Config.HatMode == 2 then
-
-				Hats[ a ]:Destroy( )
-
+				Hat:Destroy()
 			else
-
-				Hats[ a ].Parent = workspace
-
-				local Reset
-
-				local Event = Hats[ a ].AncestryChanged:Connect( function ( )
-
-					Reset = true
-
-				end )
-
-				delay( 5, function ( )
-
-					if not Reset then
-
-						Hats[ a ]:Destroy( )
-
+				Hat.Parent = workspace
+				
+				local Event; Event = Hat.AncestryChanged:Connect(function()
+					Event:Disconnect()
+					Event = nil
+				end)
+				
+				delay(5, function()
+					if Event then
+						Hat:Destroy()
 					end
-
-				end )
-
+				end)
 			end
-
 		end
-
 	end
+end)
 
-end )
-
-DropHatRemote.Parent = game:GetService( "ReplicatedStorage" ):WaitForChild( "S2" )
+DropHatRemote.Parent = game:GetService("ReplicatedStorage"):WaitForChild("S2")
