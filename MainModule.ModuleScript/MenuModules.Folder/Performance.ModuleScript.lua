160a6012f4448b6569154ee8af5ff9fed10f402b
Nick (Partixel) Lock <partixel@v-handle.com>
2019-10-18 17:17:46 +0100

    Removed a debug print

--- MainModule.ModuleScript/MenuModules.Folder/Performance.ModuleScript.lua
+++ MainModule.ModuleScript/MenuModules.Folder/Performance.ModuleScript.lua
@@ -6,7 +6,6 @@ return {
 	AllowRemoteSet = true,
 	BeforeRemoteSet = function(Plr, DataStore, Remote, Setting, Value)
 		local Data = DataStore:Get({})
-		print(Value)
 		Data[Setting] = Value
 		return next(Data) and Data or nil
 	end,
