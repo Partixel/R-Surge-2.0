bda96c148152d1ba30494f9146c73c6bc22dc114
Nick (Partixel) Lock <partixel@v-handle.com>
2019-10-23 20:30:32 +0100

    Optimised character rotation
    Signed-off-by: Nick (Partixel) Lock <partixel@v-handle.com>

--- MainModule.ModuleScript/StarterPlayerScripts.Folder/CharacterRotation.LocalScript.lua
+++ MainModule.ModuleScript/StarterPlayerScripts.Folder/CharacterRotation.LocalScript.lua
@@ -1,49 +1,40 @@
-local Core = require( game:GetService( "ReplicatedStorage" ):WaitForChild( "S2" ):WaitForChild( "Core" ) )
-
-local Plr = game:GetService( "Players" ).LocalPlayer
-
-local Until
+local Core = require(game:GetService("ReplicatedStorage"):WaitForChild("S2"):WaitForChild("Core"))
+local Plr = game:GetService("Players").LocalPlayer
 
 Core.PreventCharacterRotation = { }
 
-Core.Visuals.CharacterRotation = Core.ClientVisuals.Event:Connect( function ( StatObj )
-	
-	if Core.Config.AllowCharacterRotation == false then return end
-	
-	local Weapon = Core.GetWeapon( StatObj )
-	
-	local HumanoidRootPart, Humanoid = Plr.Character and Plr.Character:FindFirstChild( "HumanoidRootPart" ), Plr.Character and Plr.Character:FindFirstChildOfClass( "Humanoid" )
-	
-	local ValidState = Humanoid and ( Humanoid:GetState( ) == Enum.HumanoidStateType.FallingDown or Humanoid:GetState( ) == Enum.HumanoidStateType.Flying or Humanoid:GetState( ) == Enum.HumanoidStateType.Freefall or Humanoid:GetState( ) == Enum.HumanoidStateType.Jumping or Humanoid:GetState( ) == Enum.HumanoidStateType.Landed or Humanoid:GetState( ) == Enum.HumanoidStateType.Running or Humanoid:GetState( ) == Enum.HumanoidStateType.RunningNoPhysics or Humanoid:GetState( ) == Enum.HumanoidStateType.StrafingNoPhysics )
-	
-	if Weapon and HumanoidRootPart and ValidState then
-		
-		if not Until then
-			
-			Until = tick( ) + 5
-			
-			local Event Event = game:GetService( "RunService" ).Stepped:Connect( function ( )
+local Event, Until
+function UpdateCharacterRotation(StatObj)
+	local Weapon = Core.GetWeapon(StatObj)
+	if Weapon then
+		local HumanoidRootPart, Humanoid = Plr.Character and Plr.Character:FindFirstChild("HumanoidRootPart"), Plr.Character and Plr.Character:FindFirstChildOfClass("Humanoid")
+		if HumanoidRootPart and Humanoid and (Humanoid:GetState() == Enum.HumanoidStateType.FallingDown or Humanoid:GetState() == Enum.HumanoidStateType.Flying or Humanoid:GetState() == Enum.HumanoidStateType.Freefall or Humanoid:GetState() == Enum.HumanoidStateType.Jumping or Humanoid:GetState() == Enum.HumanoidStateType.Landed or Humanoid:GetState() == Enum.HumanoidStateType.Running or Humanoid:GetState() == Enum.HumanoidStateType.RunningNoPhysics or Humanoid:GetState() == Enum.HumanoidStateType.StrafingNoPhysics) then
+			if not Until then
+				Until = tick() + 5
 				
-				if Until < tick( ) or not Core.Selected[ Weapon.User ] or not Core.Selected[ Weapon.User ][ Weapon ] then Event:Disconnect( ) Until = nil return end
-				
-				if not HumanoidRootPart or HumanoidRootPart.Anchored or ( workspace.CurrentCamera.CoordinateFrame.p - workspace.CurrentCamera.Focus.p ).magnitude <= 0.55 then return end
-				
-				if next( Core.PreventCharacterRotation ) then return end
-				
-				ValidState = Humanoid and ( Humanoid:GetState( ) == Enum.HumanoidStateType.FallingDown or Humanoid:GetState( ) == Enum.HumanoidStateType.Flying or Humanoid:GetState( ) == Enum.HumanoidStateType.Freefall or Humanoid:GetState( ) == Enum.HumanoidStateType.Jumping or Humanoid:GetState( ) == Enum.HumanoidStateType.Landed or Humanoid:GetState( ) == Enum.HumanoidStateType.Running or Humanoid:GetState( ) == Enum.HumanoidStateType.RunningNoPhysics or Humanoid:GetState( ) == Enum.HumanoidStateType.StrafingNoPhysics )
-				
-				if not ValidState then return end
-				
-				HumanoidRootPart.CFrame = CFrame.new( HumanoidRootPart.Position, Vector3.new( Core.LPlrsTarget[ 2 ].X, HumanoidRootPart.Position.Y, Core.LPlrsTarget[ 2 ].Z ) )
-				
-			end )
-			
-		else
-			
-			Until = tick( ) + 5
-			
+				local SteppedEvent SteppedEvent = game:GetService("RunService").Stepped:Connect(function()
+					if not Event or not HumanoidRootPart.Parent or not Humanoid.Parent or Until < tick() or not Core.Selected[Weapon.User] or not Core.Selected[Weapon.User][Weapon] then
+						SteppedEvent:Disconnect()
+						Until = nil
+					elseif not HumanoidRootPart.Anchored and (workspace.CurrentCamera.CoordinateFrame.p - workspace.CurrentCamera.Focus.p).magnitude > 0.55 and not next(Core.PreventCharacterRotation) and (Humanoid:GetState() == Enum.HumanoidStateType.FallingDown or Humanoid:GetState() == Enum.HumanoidStateType.Flying or Humanoid:GetState() == Enum.HumanoidStateType.Freefall or Humanoid:GetState() == Enum.HumanoidStateType.Jumping or Humanoid:GetState() == Enum.HumanoidStateType.Landed or Humanoid:GetState() == Enum.HumanoidStateType.Running or Humanoid:GetState() == Enum.HumanoidStateType.RunningNoPhysics or Humanoid:GetState() == Enum.HumanoidStateType.StrafingNoPhysics) then
+						HumanoidRootPart.CFrame = CFrame.new(HumanoidRootPart.Position, Vector3.new(Core.LPlrsTarget[ 2 ].X, HumanoidRootPart.Position.Y, Core.LPlrsTarget[ 2 ].Z))
+					end
+				end)
+			else
+				Until = tick() + 5
+			end
 		end
-		
 	end
-	
-end )
+end
+
+local Menu = require(game:GetService("ReplicatedStorage"):WaitForChild("MenuLib"):WaitForChild("S2"))
+Menu:AddSetting{Name = "CharacterAim", Text = "Rotate character towards target", Default = true, Update = function(Options, Val)
+	if Val then
+		if not Event then
+			Event = Core.ClientVisuals.Event:Connect(UpdateCharacterRotation)
+		end
+	elseif Event then
+		Event:Disconnect()
+		Event = nil
+	end
+end}
