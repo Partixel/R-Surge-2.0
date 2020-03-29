378dfbec1f8daa8a479d967f416e571c41ba7d37
Nick (Partixel) Lock <partixel@v-handle.com>
2020-03-29 11:38:45 +0100

    Fixed an error in TopBar if it was overwritten

--- MainModule.ModuleScript/StarterGui.Folder/TopBar.ScreenGui/TopBar.LocalScript.lua
+++ MainModule.ModuleScript/StarterGui.Folder/TopBar.ScreenGui/TopBar.LocalScript.lua
@@ -1,23 +1,23 @@
-local ThemeUtil = require( game:GetService( "ReplicatedStorage" ):WaitForChild( "ThemeUtil" ):WaitForChild( "ThemeUtil" ) )
-
-ThemeUtil.BindUpdate( script.Parent.Frame, { BackgroundColor3 = "Primary_BackgroundColor", BackgroundTransparency = "Primary_BackgroundTransparency" } )
-
-game:GetService( "Players" ).LocalPlayer:WaitForChild( "PlayerGui" ):SetTopbarTransparency( 1 )
-
---[[local PlayerGui = game:GetService( "Players" ).LocalPlayer:WaitForChild( "PlayerGui" )
-
-script.Parent.Frame.BackgroundTransparency = PlayerGui:GetTopbarTransparency( )
-
-PlayerGui:SetTopbarTransparency( 1 )
-
---[[local Changed
-
-PlayerGui.TopbarTransparencyChangedSignal:Connect( function ( Transparency )
+local ThemeUtil = require(game:GetService("ReplicatedStorage"):WaitForChild("ThemeUtil"):WaitForChild("ThemeUtil"))
+if script.Parent then
+	ThemeUtil.BindUpdate(script.Parent.Frame, {BackgroundColor3 = "Primary_BackgroundColor", BackgroundTransparency = "Primary_BackgroundTransparency"})
+	game:GetService("Players").LocalPlayer:WaitForChild("PlayerGui"):SetTopbarTransparency(1)
 	
-	if Changed then Changed = nil return end
+	--[[local PlayerGui = game:GetService( "Players" ).LocalPlayer:WaitForChild( "PlayerGui" )
 	
-	script.Parent.Frame.BackgroundTransparency = Transparency
+	script.Parent.Frame.BackgroundTransparency = PlayerGui:GetTopbarTransparency( )
 	
 	PlayerGui:SetTopbarTransparency( 1 )
 	
-end )]]
+	local Changed
+	
+	PlayerGui.TopbarTransparencyChangedSignal:Connect( function ( Transparency )
+		
+		if Changed then Changed = nil return end
+		
+		script.Parent.Frame.BackgroundTransparency = Transparency
+		
+		PlayerGui:SetTopbarTransparency( 1 )
+		
+	end )]]
+end
