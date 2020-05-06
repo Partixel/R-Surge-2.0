af500f39b01d05efd58bfef8610ad81da1cccc22
Nick (Partixel) Lock <partixel@v-handle.com>
2020-05-06 14:13:53 +0100

    Fixed FirstPersonMod

--- MainModule.ModuleScript/StarterCharacterScripts.Folder/FirstPersonMod.LocalScript.lua
+++ MainModule.ModuleScript/StarterCharacterScripts.Folder/FirstPersonMod.LocalScript.lua
@@ -1,39 +1,24 @@
-local Handled = setmetatable( { }, { __mode = "k" } )
-
-function HandlePart( Part )
-	
-	if Part:IsA( "BasePart" ) and ( Part.Name:lower( ):find( "leg" ) or Part.Name:lower( ):find( "arm" ) or Part.Name == "LeftHand" or Part.Name == "RightHand" or Part.Name == "LeftFoot" or Part.Name == "RightFoot" ) then
-		
-		Handled[ Part ] = true
-		
-		local Event Event = Part:GetPropertyChangedSignal( "LocalTransparencyModifier" ):Connect( function ( )
-			
-			if not Part:IsDescendantOf( script.Parent ) then Event:Disconnect( ) return end
-			
-			Part.LocalTransparencyModifier = 0
-			
-		end )
-		
+local Handled = setmetatable({}, {__mode = "k"})
+function HandlePart(Part)
+	if Part:IsA("BasePart") and (Part.Name:lower():find("leg") or Part.Name:lower():find("arm") or Part.Name == "LeftHand" or Part.Name == "RightHand" or Part.Name == "LeftFoot" or Part.Name == "RightFoot") then
+		Handled[Part] = true
+		local Event Event = Part:GetPropertyChangedSignal("LocalTransparencyModifier"):Connect(function()
+			if Part:IsDescendantOf(script.Parent.Parent) then
+				Part.LocalTransparencyModifier = 0
+			else
+				Event:Disconnect()
+			end
+		end)
 	end
-	
 end
 
-script.Parent.DescendantAdded:Connect( function ( Obj )
-	
-	if not Handled[ Obj ] then
-		
-		HandlePart( Obj )
-		
+script.Parent.Parent.DescendantAdded:Connect(function(Obj)
+	if not Handled[Obj] then
+		HandlePart(Obj)
 	end
-	
-end )
-
-for _, Obj in ipairs( script.Parent:GetDescendants( )) do
-	
-	if not Handled[ Obj ] then
-		
-		HandlePart( Obj )
-		
+end)
+for _, Obj in ipairs(script.Parent.Parent:GetDescendants()) do
+	if not Handled[Obj] then
+		HandlePart(Obj)
 	end
-	
 end
