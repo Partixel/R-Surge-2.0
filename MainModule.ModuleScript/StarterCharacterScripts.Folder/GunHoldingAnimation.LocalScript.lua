7483fdc3fbb32d3cbb4ab0b5d24b69c61c2359f0
Nick (Partixel) Lock <partixel@v-handle.com>
2020-05-06 21:03:47 +0100

    Update gun holding animation to use AnimationWrapper

--- MainModule.ModuleScript/StarterCharacterScripts.Folder/GunHoldingAnimation.LocalScript.lua
+++ MainModule.ModuleScript/StarterCharacterScripts.Folder/GunHoldingAnimation.LocalScript.lua
@@ -1,24 +1,14 @@
 local Core = require(game:GetService("ReplicatedStorage"):WaitForChild("S2"):WaitForChild("Core"))
-local Humanoid = script.Parent.Parent:WaitForChild("Humanoid")
-local Animations = {}
-local function GetAnimation(AnimProps)
-	if not Animations[AnimProps] then
-		local Animation = Instance.new("Animation")
-		Animation.AnimationId = "rbxassetid://" .. AnimProps.Id
-		Animations[AnimProps] = Humanoid:LoadAnimation(Animation)
-	end
-	
-	return Animations[AnimProps]
-end
+local AnimationWrapper = require(script.Parent:WaitForChild("AnimationWrapper"))
 
 Core.WeaponSelected.Event:Connect(function(StatObj)
 	local Weapon = Core.GetWeapon(StatObj)
 	if Weapon then
-		local Animation = Weapon[Humanoid.RigType.Name .. "HoldAnimation"]
+		local Animation = Weapon[AnimationWrapper.Humanoid.RigType.Name .. "HoldAnimation"]
 		if Animation then
-			Animation = GetAnimation(Animation)
+			Animation = AnimationWrapper.GetAnimation("HoldAnim", Animation, 1)
 			Weapon.HoldAnimation = Animation
-			Animation:Play(nil, 1)
+			Animation:Play()
 		end
 	end
 end)
