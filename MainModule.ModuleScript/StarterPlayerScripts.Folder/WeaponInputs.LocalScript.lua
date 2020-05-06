22a891a0297f111b6578898be307011ab6d577c6
Nick (Partixel) Lock <partixel@v-handle.com>
2020-05-06 13:52:33 +0100

    Fixed bug where weapon would keep attacking if mouse up event triggered as handled

--- MainModule.ModuleScript/StarterPlayerScripts.Folder/WeaponInputs.LocalScript.lua
+++ MainModule.ModuleScript/StarterPlayerScripts.Folder/WeaponInputs.LocalScript.lua
@@ -1,19 +1,20 @@
-local LocalPlayer = game:GetService( "Players" ).LocalPlayer
+local LocalPlayer = game:GetService("Players").LocalPlayer
 
-local Core = require( game:GetService( "ReplicatedStorage" ):WaitForChild( "S2" ):WaitForChild( "Core" ) )
+local Core = require(game:GetService("ReplicatedStorage"):WaitForChild("S2"):WaitForChild("Core"))
+local KBU = require(game:GetService("Players").LocalPlayer:WaitForChild("PlayerScripts"):WaitForChild("S2"):WaitForChild("KeybindUtil"))
 
-local KBU = require( game:GetService( "Players" ).LocalPlayer:WaitForChild( "PlayerScripts" ):WaitForChild( "S2" ):WaitForChild( "KeybindUtil" ) )
-
-KBU.AddBind{ Name = "Fire", Category = "Surge 2.0", Callback = function (Began)
-	local Weapons = Core.Selected[ LocalPlayer ]
-	if Weapons then
-		for a, _ in pairs( Weapons ) do
-			if not a.ManualFire then
-				(Began and Core.SetMouseDown or Core.SetMouseUp)(a)
+KBU.AddBind{Name = "Fire", Category = "Surge 2.0", Callback = function(Began, Handled)
+	if not Began or not Handled then
+		local Weapons = Core.Selected[ LocalPlayer ]
+		if Weapons then
+			for a, _ in pairs( Weapons ) do
+				if not a.ManualFire then
+					(Began and Core.SetMouseDown or Core.SetMouseUp)(a)
+				end
 			end
 		end
 	end
-end, Key = Enum.UserInputType.MouseButton1, PadKey = Enum.KeyCode.ButtonR2, NoHandled = true}
+end, Key = Enum.UserInputType.MouseButton1, PadKey = Enum.KeyCode.ButtonR2}
 
 KBU.AddBind{Name = "Reload", Category = "Surge 2.0", Callback = function(Held, Died)
 	local Weapons = Core.Selected[LocalPlayer]
@@ -26,7 +27,7 @@ KBU.AddBind{Name = "Reload", Category = "Surge 2.0", Callback = function(Held, D
 	end
 end, Key = Enum.KeyCode.R, PadKey = Enum.KeyCode.ButtonB, NoHandled = true, HoldFor = 0.5}
 
-KBU.AddBind{Name = "Next_weapon_mode", Category = "Surge 2.0", Callback = function (Began)
+KBU.AddBind{Name = "Next_weapon_mode", Category = "Surge 2.0", Callback = function(Began)
 	if Began then
 		local Weapons = Core.Selected[ LocalPlayer ]
 		if Weapons then
