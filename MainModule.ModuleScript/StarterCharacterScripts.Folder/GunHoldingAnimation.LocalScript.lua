3b53c9cc2c6e889cdec6b3b66d82a678fe6a3ae7
Nick (Partixel) Lock <partixel@v-handle.com>
2020-05-07 18:16:07 +0100

    Optimised hold animation slihtlyOptimise hold animation

--- MainModule.ModuleScript/StarterCharacterScripts.Folder/GunHoldingAnimation.LocalScript.lua
+++ MainModule.ModuleScript/StarterCharacterScripts.Folder/GunHoldingAnimation.LocalScript.lua
@@ -1,21 +1,20 @@
 local Core = require(game:GetService("ReplicatedStorage"):WaitForChild("S2"):WaitForChild("Core"))
 local AnimationWrapper = require(script.Parent:WaitForChild("AnimationWrapper"))
 
+local HoldAnimation
 Core.WeaponSelected.Event:Connect(function(StatObj)
 	local Weapon = Core.GetWeapon(StatObj)
 	if Weapon then
-		local Animation = Weapon[AnimationWrapper.Humanoid.RigType.Name .. "HoldAnimation"]
-		if Animation then
-			Animation = AnimationWrapper.GetAnimation("HoldAnim", Animation, 1)
-			Weapon.HoldAnimation = Animation
-			Animation:Play()
+		HoldAnimation = Weapon[AnimationWrapper.Humanoid.RigType.Name .. "HoldAnimation"]
+		if HoldAnimation then
+			HoldAnimation = AnimationWrapper.GetAnimation("Hold", HoldAnimation, 1)
+			HoldAnimation:Play()
 		end
 	end
 end)
 
 Core.WeaponDeselected.Event:Connect(function(StatObj)
-	local Weapon = Core.GetWeapon(StatObj)
-	if Weapon and Weapon.HoldAnimation then
-		Weapon.HoldAnimation = Weapon.HoldAnimation:Stop()
+	if HoldAnimation then
+		HoldAnimation = HoldAnimation:Stop()
 	end
 end)
