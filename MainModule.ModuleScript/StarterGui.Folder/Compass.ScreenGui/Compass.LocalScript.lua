ad5450eb47ae1be687a3bd28ab3c8b1e581e6911
Nick (Partixel) Lock <partixel@v-handle.com>
2020-05-14 13:56:59 +0100

    Added Compass GUI

---
+++ MainModule.ModuleScript/StarterGui.Folder/Compass.ScreenGui/Compass.LocalScript.lua
@@ -0,0 +1,124 @@
+local CollectionService = game:GetService("CollectionService")
+
+local ThemeUtil = require(game:GetService("ReplicatedStorage"):WaitForChild("ThemeUtil"):WaitForChild("ThemeUtil"))
+
+local DirectionNames = {
+	[0] = "N",
+	[22.5] = "NNE",
+	[45] = "NE",
+	[67.5] = "ENE",
+	[90] = "E",
+	[112.5] = "ESE",
+	[135] = "SE",
+	[157.5] = "SSE",
+	[180] = "S",
+	[202.5] = "SSW",
+	[225] = "SW",
+	[247.5] = "WSW",
+	[270] = "W",
+	[292.5] = "WNW",
+	[315] = "NW",
+	[337.5] = "NNW",
+}
+local Degrees = 5.625
+local AngleToShow = 30
+
+ThemeUtil.BindUpdate({script.Parent.CompassFrame.BottomArrow, script.Parent.CompassFrame.TopArrow}, {TextColor3 = "Primary_BackgroundColor", TextStrokeColor3 = "Inverted_BackgroundColor"})
+ThemeUtil.BindUpdate(script.Parent.CompassFrame.Bar, {ImageColor3 = "Primary_BackgroundColor"})
+ThemeUtil.BindUpdate(script.Parent.CompassFrame.BackBar, {ImageColor3 = "Inverted_BackgroundColor"})
+
+function GetAngle(CFrameA, VectorB)
+	local Projected = (CFrameA * CFrame.Angles(0, math.rad(90), 0)):PointToObjectSpace(VectorB)
+	local Angle = math.deg(math.atan2(Projected.Z, Projected.X))
+	if Angle < 0 then
+		return 360 + Angle
+	else
+		return Angle
+	end
+end
+
+local Angles = {}
+for i = 0, 360 - Degrees, Degrees do
+	local Angle = i == 0 and 0 or 360 - i
+	
+	local Point = script.Point:Clone()
+	Point.Name = Angle
+	Point.Degrees.Text = math.floor(Angle + 0.5)
+	Point.DirectionName.Text = DirectionNames[Angle] or ".."
+	
+	ThemeUtil.BindUpdate({Point.Degrees, Point.DirectionName}, {TextColor3 = "Primary_TextColor", TextStrokeColor3 = "Inverted_TextColor"})
+	
+	Point.Parent = script.Parent.CompassFrame
+		
+	Angles[(CFrame.Angles(0, math.rad(90), 0) * CFrame.Angles(0, math.rad(i), 0) * CFrame.new(0, 0, 1)).p] = Point
+end
+
+local POI = {}
+local function AddPOI(Part)
+	local ObjPoint = script.ObjPoint:Clone()
+	ObjPoint.Name = Part.Name
+	ObjPoint.TextColor3 = Part.Color
+	ObjPoint.Parent = script.Parent.CompassFrame
+	
+	ThemeUtil.BindUpdate(ObjPoint, {TextStrokeColor3 = "Inverted_BackgroundColor"})
+	
+	POI[Part] = ObjPoint
+end
+
+for _, Part in ipairs(CollectionService:GetTagged("S2_POI")) do
+	AddPOI(Part)
+end
+CollectionService:GetInstanceAddedSignal("S2_POI"):Connect(AddPOI)
+CollectionService:GetInstanceRemovedSignal("S2_POI"):Connect(function(Part)
+	POI[Part] = POI[Part]:Destroy()
+end)
+
+for i = 1, 2 do
+	local Part = Instance.new("Part")
+	Part.Name = "Part" .. i
+	Part.Color = BrickColor.Random().Color
+	Part.Anchored = true
+	Part.CanCollide = false
+	Part.Size = Vector3.new(0.5, 0.5, 0.5)
+	Part.Parent = workspace
+	
+	CollectionService:AddTag(Part, "S2_POI")
+	
+	spawn(function()
+		local Angle = (i - 1) * 45
+		while game["Run Service"].RenderStepped:Wait() do
+			Part.CFrame = workspace.CurrentCamera.Focus * CFrame.Angles(0, math.rad(180), 0) * CFrame.Angles(0, math.rad(Angle), 0) * CFrame.new(0, 0, 5)
+			Angle = Angle + (i % 2 == 0 and -0.1 or 0.1)
+		end
+	end)
+end
+
+while game["Run Service"].Heartbeat:Wait() do
+	local CameraLookVector = workspace.CurrentCamera.CFrame.LookVector * Vector3.new(1, 0, 1)
+	
+	for Angle, Point in pairs(Angles) do
+		Angle = GetAngle(CFrame.new(Vector3.new(), CameraLookVector), Angle)
+		script.Parent.PositionalFrame.Rotation = Angle
+		Point.Visible = Angle <= AngleToShow or Angle >= 360 - AngleToShow - 1
+		if Point.Visible then
+			Point.Position = UDim2.new(0, script.Parent.PositionalFrame.TextLabel.AbsolutePosition.X - (workspace.CurrentCamera.ViewportSize.X - script.Parent.CompassFrame.AbsoluteSize.X) / 2, 0.5, 0)
+			local Pct = (Angle <= AngleToShow and Angle / AngleToShow or (360 - Angle) / AngleToShow)
+			Point.Degrees.TextStrokeTransparency = 0.05 + Pct * 0.95
+			Point.Degrees.TextTransparency = Pct
+			Point.DirectionName.TextStrokeTransparency = 0.05 + Pct * 0.95
+			Point.DirectionName.TextTransparency = Pct
+		end
+	end
+	
+	for Part, ObjPoint in pairs(POI) do
+		local Angle = GetAngle(CFrame.new(workspace.CurrentCamera.Focus.p, workspace.CurrentCamera.Focus.p + CameraLookVector), Part.Position)
+		script.Parent.PositionalFrame.Rotation = Angle
+		ObjPoint.Visible = Angle <= AngleToShow or Angle >= 360 - AngleToShow - 1
+		if ObjPoint.Visible then
+			ObjPoint.Position = UDim2.new(0, script.Parent.PositionalFrame.TextLabel.AbsolutePosition.X - (workspace.CurrentCamera.ViewportSize.X - script.Parent.CompassFrame.AbsoluteSize.X) / 2, 0.485, 0)
+			local Pct = (Angle <= AngleToShow and Angle / AngleToShow or (360 - Angle) / AngleToShow)
+			ObjPoint.TextStrokeTransparency = 0.35 + Pct * 0.65
+			ObjPoint.TextTransparency = Pct
+		end
+	end
+end
