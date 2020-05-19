8a6bdae713fb30c4342249170eab05d57afbb3eb
Nick (Partixel) Lock <partixel@v-handle.com>
2020-05-19 13:47:05 +0100

    Updated Config

--- S2.Folder/Config.ModuleScript.lua
+++ S2.Folder/Config.ModuleScript.lua
@@ -1,6 +1,6 @@
 return {
 	--[[Uses Major.minor.patch format, if a number is specified instead of * it'll restrict the version to that number]]
-	Version = "6.*.*", -- Default - "5.*.*"
+	Version = "6.*.*", -- Default - "6.*.*"
 	
 	--[[If the name of an object S2 loads in (e.g. ["Hud"] = true) is in this table it won't load said object]]
 	Disabled = { }, -- Default - { }
@@ -11,19 +11,7 @@ return {
 	
 	--[[Teams that cannot team kill each other e.g. { { [ "Bright green" ] = true , White = true } }
 	will stop teams Bright green and White from killing each other]]
-	AllowTeamKillFor = { 
-		
-		{ 
-			
-			[ "Bright green" ] = true,
-			
-			Black = true,
-			
-			[ "Bright yellow" ] = true,
-			
-		},
-		
-	}, -- Default - { }
+	AllowTeamKillFor = { }, -- Default - { }
 	
 	--------[[ Gameplay Configs ]]--------
 	
@@ -35,48 +23,62 @@ return {
 		
 		All = { 
 			
-			LimbDamageMultiplier = 0.9,
+			AllowCrouch = true,
 			
-			DistanceDamageModifier = 0.2,
+			CrouchSpeedMultiplier = 0.9,
 			
-			AllowSelfDamage = false,
+			SprintSpeedMultiplier = 1.35,
 			
 			AllowNeutralTeamKill = true,
 			
 			AllowTeamKill = false,
 			
-			ShotKnockbackPercentage = 1,
-			
 			GlobalDamageMultiplier = 1,
 			
 			MovementAccuracyPercentage = 1,
 			
-			HeadDamageMultiplier = 1.75,
+			LimbDamageMultiplier = 0.9,
+			
+			AllowAtEase = true,
+			
+			R6InspectAnimation = 5052440268,
+			
+			AllowInspect = true,
+			
+			DistanceDamageModifier = 0.2,
+			
+			AllowSelfDamage = false,
+			
+			R6AtEaseAnimation = 955877742,
+			
+			CrouchJumpPowerMultiplier = 0.6,
 			
 			ScreenRecoilPercentage = 1,
 			
 			Resistances = { },
 			
+			ShotKnockbackPercentage = 1,
+			
+			HeadDamageMultiplier = 1.75,
+			
 			AllowSprint = true,
-			
-			SprintSpeedMultiplier = 1.35,
-			
-			AllowAtEase = true,
-			
-			AllowCrouch = true,
-			
-			CrouchSpeedMultiplier = 0.9,
-			
-			CrouchJumpPowerMultiplier = 0.6,
 			
 		},
 		
-	}, -- Default - { All = { LimbDamageMultiplier = 0.9, DistanceDamageModifier = 0.2, AllowSelfDamage = false, AllowNeutralTeamKill = true, AllowTeamKill = false, ShotKnockbackPercentage = 1, Resistances = { }, MovementAccuracyPercentage = 1, HeadDamageMultiplier = 1.75, ScreenRecoilPercentage = 1, GlobalDamageMultiplier = 1, }, }
+	}, -- Default - { All = { AllowCrouch = true, CrouchSpeedMultiplier = 0.9, SprintSpeedMultiplier = 1.35, AllowNeutralTeamKill = true, AllowTeamKill = false, GlobalDamageMultiplier = 1, MovementAccuracyPercentage = 1, LimbDamageMultiplier = 0.9, AllowAtEase = true, R6InspectAnimation = 5052440268, AllowInspect = true, DistanceDamageModifier = 0.2, AllowSelfDamage = false, R6AtEaseAnimation = 955877742, CrouchJumpPowerMultiplier = 0.6, ScreenRecoilPercentage = 1, Resistances = { }, ShotKnockbackPercentage = 1, HeadDamageMultiplier = 1.75, AllowSprint = true, }, }
 	
 	--[[Automatically handle arm welds]]
 	ArmWelds = true, -- Default - true
 	
+	R6SaluteAnimation = 580605334, -- Default - 580605334
+	
+	R15SaluteAnimation = nil, -- Default - { }
+	
 	AllowSalute = true, -- Default - true
+	
+	R6SurrenderAnimation = 1173354695, -- Default - 1173354695
+	
+	R15SurrenderAnimation = nil, -- Default - { }
 	
 	AllowSurrender = true, -- Default - true
 	
@@ -92,7 +94,7 @@ return {
 	--[[Adds the number of players on a team after the teams name, will require scripts that access teams to be updated to do:
 	game.Teams[ "S2_TEAMNAME" ].Value instead of game.Teams.TEAMNAME
 	If this is a string it'll be used as a template for the team names (e.g. "%PlayerCount% Player(s) on %TeamName%" will translate to "2 Player(s) on Red Team" assuming the teams name is "Red Team" and it has 2 players on it]]
-	TeamCounts = true, -- Default - false
+	TeamCounts = false, -- Default - false
 	
 	--------[[ Leaderboard Configs ]]--------
 	
