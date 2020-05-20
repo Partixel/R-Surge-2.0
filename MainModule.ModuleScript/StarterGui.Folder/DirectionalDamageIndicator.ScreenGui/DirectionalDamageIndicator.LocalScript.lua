e99044af288105aa78bf6ad5aa45fe9f5232a025
Nick (Partixel) Lock <partixel@v-handle.com>
2020-05-20 16:43:27 +0100

    Added a setting to enable/disable directional damage indicator

--- MainModule.ModuleScript/StarterGui.Folder/DirectionalDamageIndicator.ScreenGui/DirectionalDamageIndicator.LocalScript.lua
+++ MainModule.ModuleScript/StarterGui.Folder/DirectionalDamageIndicator.ScreenGui/DirectionalDamageIndicator.LocalScript.lua
@@ -13,41 +13,50 @@ function GetAngle(CFrameA, VectorB)
 	end
 end
 
-local Menu = require(game:GetService("ReplicatedStorage"):WaitForChild("MenuLib"):WaitForChild("S2"))
-local Enabled
-Menu:AddSetting{Name = "DirectionalDamageIndicator", Text = "Enables/disables the directional damage indicator", Default = true, Update = function(Options, Val)
-	Enabled = Val
-end}
-
 local MinY = 0.5
-Core.Events.DirectionalDamageIndicator = Core.ClientDamage.OnClientEvent:Connect(function(DamageSplits, Str, ExtraInformation)
-	if type(Str) == "string" and Enabled and ExtraInformation.StartPosition then
-		local HumanoidRootPart = LocalPlayer.Character and LocalPlayer.Character:FindFirstChild("HumanoidRootPart")
-		if HumanoidRootPart then
-			local TotalDamage = 0
-			for _, DamageSplit in ipairs(DamageSplits) do
-				TotalDamage = TotalDamage + DamageSplit[2]
+
+local DamageEvent
+local function ConnectDamageEvent()
+	DamageEvent = Core.ClientDamage.OnClientEvent:Connect(function(DamageSplits, Str, ExtraInformation)
+		if type(Str) == "string" and ExtraInformation.StartPosition then
+			local HumanoidRootPart = LocalPlayer.Character and LocalPlayer.Character:FindFirstChild("HumanoidRootPart")
+			if HumanoidRootPart then
+				local TotalDamage = 0
+				for _, DamageSplit in ipairs(DamageSplits) do
+					TotalDamage = TotalDamage + DamageSplit[2]
+				end
+				
+				local DamageVisual = script.Container:Clone()
+				local DmgPerc = math.min(TotalDamage / 100, 1)
+				DamageVisual.Size = UDim2.new((MinY + MinY * DmgPerc * 2) * 0.76, 0, MinY + MinY * DmgPerc * 2, 0)
+						
+				local DamageAngle = GetAngle(CFrame.new(HumanoidRootPart.CFrame.p, HumanoidRootPart.CFrame.p + workspace.CurrentCamera.CFrame.LookVector * Vector3.new(1, 0, 1)), ExtraInformation.StartPosition)
+				DamageVisual.Rotation = DamageAngle
+				DamageVisual.ImageLabel.ImageTransparency = 1
+				
+				DamageVisual.Parent = script.Parent
+				
+				TweenService:Create(DamageVisual.ImageLabel, TweenInfo.new(0.2), {ImageTransparency = 0}):Play()
+				
+				wait(0.1)
+				
+				local Tween = TweenService:Create(DamageVisual.ImageLabel, TweenInfo.new(0.3), {ImageTransparency = 1})
+				Tween:Play()
+				Tween.Completed:Wait()
+				
+				DamageVisual:Destroy()
 			end
-			
-			local DamageVisual = script.Container:Clone()
-			local DmgPerc = math.min(TotalDamage / 100, 1)
-			DamageVisual.Size = UDim2.new((MinY + MinY * DmgPerc * 2) * 0.76, 0, MinY + MinY * DmgPerc * 2, 0)
-					
-			local DamageAngle = GetAngle(CFrame.new(HumanoidRootPart.CFrame.p, HumanoidRootPart.CFrame.p + workspace.CurrentCamera.CFrame.LookVector * Vector3.new(1, 0, 1)), ExtraInformation.StartPosition)
-			DamageVisual.Rotation = DamageAngle
-			DamageVisual.ImageLabel.ImageTransparency = 1
-			
-			DamageVisual.Parent = script.Parent
-			
-			TweenService:Create(DamageVisual.ImageLabel, TweenInfo.new(0.2), {ImageTransparency = 0}):Play()
-			
-			wait(0.1)
-			
-			local Tween = TweenService:Create(DamageVisual.ImageLabel, TweenInfo.new(0.3), {ImageTransparency = 1})
-			Tween:Play()
-			Tween.Completed:Wait()
-			
-			DamageVisual:Destroy()
 		end
+	end)
+end
+
+local Menu = require(game:GetService("ReplicatedStorage"):WaitForChild("MenuLib"):WaitForChild("S2"))
+Menu:AddSetting{Name = "DirectionalDamageIndicator", Text = "Enables/disables the directional damage indicator", Default = true, Update = function(Options, Val)
+	if Val then
+		if not DamageEvent then
+			ConnectDamageEvent()
+		end
+	elseif DamageEvent then
+		DamageEvent = DamageEvent:Disconnect()
 	end
-end)
+end}
