f0e87f54b59e61cd97188e7f23cf869c82b73030
Nick (Partixel) Lock <partixel@v-handle.com>
2020-10-29 17:35:39 +0000

    PartTeamVisibility now supports Models/Folders/Anything containing BaseParts

--- MainModule.ModuleScript/7.0.0.ModuleScript/StarterPlayerScripts.Folder/PartTeamVisibility.LocalScript.lua
+++ MainModule.ModuleScript/7.0.0.ModuleScript/StarterPlayerScripts.Folder/PartTeamVisibility.LocalScript.lua
@@ -1,24 +1,31 @@
 local CollectionService, PhysicsService = game:GetService("CollectionService"), game:GetService("PhysicsService")
 local LocalPlayer = game:GetService("Players").LocalPlayer
 
-local function Update()
-	print("Do team change", LocalPlayer.Team.Name)
-	for _, Part in ipairs(game.CollectionService:GetTagged("S2_TeamVisibility")) do
-		if LocalPlayer.Team then
-			local TeamName = LocalPlayer.Team:FindFirstChild("OriginalName") and string.sub(LocalPlayer.Team.OriginalName.Value.Name, 4) or LocalPlayer.Team.Name
-			if CollectionService:HasTag(Part, TeamName) then
-				Part.Transparency = 1
-				PhysicsService:SetPartCollisionGroup(Part, "S2_NoCollide")
-			else
-				Part.Transparency = 0
-				PhysicsService:SetPartCollisionGroup(Part, "Default")
+local function UpdateObject(Obj, Visible)
+	if Obj:IsA("Model") then
+		for _, Obj in ipairs(Obj:GetDescendants()) do
+			if Obj:IsA("BasePart") then
+				Obj.Transparency = Visible and 0 or 1
+				PhysicsService:SetPartCollisionGroup(Obj, Visible and "Default" or "S2_NoCollide")
 			end
-		else
-			Part.Transparency = 0
-			PhysicsService:SetPartCollisionGroup(Part, "Default")
 		end
 	end
 end
 
-game.Players.LocalPlayer:GetPropertyChangedSignal("Team"):Connect(Update)
+local function Update()
+	for _, Obj in ipairs(game.CollectionService:GetTagged("S2_TeamVisibility")) do
+		if LocalPlayer.Team then
+			local TeamName = LocalPlayer.Team:FindFirstChild("OriginalName") and string.sub(LocalPlayer.Team.OriginalName.Value.Name, 4) or LocalPlayer.Team.Name
+			if CollectionService:HasTag(Obj, TeamName) then
+				UpdateObject(Obj, false)
+			else
+				UpdateObject(Obj, true)
+			end
+		else
+			UpdateObject(Obj, true)
+		end
+	end
+end
+
+LocalPlayer:GetPropertyChangedSignal("Team"):Connect(Update)
 Update()
